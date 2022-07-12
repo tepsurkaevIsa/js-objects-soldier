92676ceef0221c55c808c68cd35b1a329db9aa82
Isa <tepsurkaev99999@mail.ru>
2022-07-12 16:11:02 +0300

    Проверь функциональность объекта. Убедись, что все методы работают как надо. Исправь недочеты, если найдешь их.

--- soldier.js
+++ soldier.js
@@ -1,41 +1,37 @@
 const soldier = {
-    name: 'Халк',
-    health: 10,
-    weapon: {
-        weaponName: 'ПП',
-        cartridges: 30,
-    },
-    purveyance: 3,
-  
-    shoot: function () {
-        if(this.weapon.cartridges){
-            this.weapon.cartridges = this.weapon.cartridges - 1
-            console.log('бах-бах')
-        } else {
-            console.log('обойма пуста. Перезаредитесь')
-        }
-        
-    },
-    recharge: function () {
-        if(this.purveyance){
-            this.weapon.cartridges = 30
-            this.purveyance--
-            console.log('перезарядка...')
-        } else {
-            console.log('не осталось припасов')
-        }
-    },
+  name: "Халк",
+  health: 10,
 
-    hurt: function () {
-        if(this.health){
-            this.health--
-        } else {
-            console.log('Ты проиграл')
-        }
-    },
-  };
+  weapon: {
+    weaponName: "ПП",
+    cartridges: 30,
+  },
+  purveyance: 3,
 
+  shoot: function () {
+    if (this.weapon.cartridges) {
+      this.weapon.cartridges = this.weapon.cartridges - 1;
 
-// солдат.ранить()
-// Данный метод после вызова должен уменьшить значение свойства здоровье на единицу.
-// Если после ранения значение здоровья стало равно нулю, то нужно вывести в консоль Ты проиграл.
+      console.log("бах-бах");
+    } else {
+      console.log("обойма пуста. Перезаредитесь");
+    }
+  },
+  recharge: function () {
+    if (this.purveyance) {
+      this.weapon.cartridges = 30;
+      this.purveyance--;
+      console.log("перезарядка...");
+    } else {
+      console.log("не осталось припасов");
+    }
+  },
+
+  hurt: function () {
+    if (this.health) {
+      this.health--;
+    } else {
+      console.log("Ты проиграл");
+    }
+  },
+};
