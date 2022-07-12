89bb784c5d5f63bbfae261a11b6339ed82cce328
Isa <tepsurkaev99999@mail.ru>
2022-07-12 14:30:08 +0300

    Создал объект soldier с информацией персонаже

---
+++ soldier.js
@@ -0,0 +1,13 @@
+const soldier = {
+    name: 'Халк',
+    health: 10,
+    weapon: {
+        weaponName: 'ПП',
+        cartridges: 30,
+    },
+    purveyance: 3,
+  
+    shoot: function () {},
+    recharge: function () {},
+    hurt: function () {},
+  };
